db7b9f29cfd60cee512e50bff06fe464464f306a
Jakub Danis <d4ny4bl3@gmail.com>
2025-05-05 01:04:58 +0200

    feat(django):  added endpoints for login and logout

--- django/accounts/views.py
+++ django/accounts/views.py
@@ -1,3 +1,44 @@
-from django.shortcuts import render
+from accounts.serializers import UserSerializer
 
-# Create your views here.
+from django.contrib.auth.models import User
+from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth.decorators import login_required
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+
+
+@api_view(["POST"])
+def login_view(request):
+    if request.user.is_authenticated:
+        user_data = UserSerializer(request.user).data
+        return Response({"user": user_data}, status=status.HTTP_200_OK)
+
+    username = request.data.get("username")
+    password = request.data.get("password")
+
+    if not username or not password:
+        return Response({"error": "Missing username or password"}, status=status.HTTP_400_BAD_REQUEST)
+
+    try:
+        user_obj = User.objects.get(username=username)
+    except User.DoesNotExist:
+        return Response({"error": "User doesn`t exits"}, status=status.HTTP_401_UNAUTHORIZED)
+
+    user = authenticate(request, username=user_obj.username, password=password)
+
+    if user is not None:
+        login(request, user)
+        user_data = UserSerializer(user).data
+        return Response({"user": user_data}, status=status.HTTP_200_OK)
+    else:
+        return Response({"error": "Invalid login"}, status=status.HTTP_401_UNAUTHORIZED)
+
+
+@api_view(["POST"])
+def logout_view(request):
+    if request.user.is_authenticated:
+        logout(request)
+        return Response({"message": "Successfully logged out"}, status=status.HTTP_200_OK)
+    else:
+        return Response({"error": "User is not authenticated"}, status=status.HTTP_401_UNAUTHORIZED)
