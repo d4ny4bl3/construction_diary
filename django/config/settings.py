99198a6a23867a13aa908c450a41280a2535ae10
Jakub Danis <d4ny4bl3@gmail.com>
2025-05-05 00:14:09 +0200

    feat(django): new django app accounts

--- django/config/settings.py
+++ django/config/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'diary',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -126,3 +127,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_URL = '/accounts/login/'
