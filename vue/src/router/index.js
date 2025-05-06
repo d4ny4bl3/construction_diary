29dd43ee5a21c1961691c83e9e7513eb81722337
Jakub Danis <danisj91@gmail.com>
2025-05-06 23:24:58 +0200

    feat(vue): router to login

---
+++ vue/src/router/index.js
@@ -0,0 +1,18 @@
+import { createRouter, createWebHistory } from "vue-router";
+
+import Login from "@/Login.vue";
+
+const routes = [
+    {
+        path: "/login",
+        name: "Login",
+        component: Login,
+    },
+]
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes
+})
+
+export default router
