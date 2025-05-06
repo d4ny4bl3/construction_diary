5c0880e66fe750bd9f4d102813712b5f0175492e
Jakub Danis <danisj91@gmail.com>
2025-05-06 23:23:00 +0200

    feat(vue): add libraries bootstrap, pinia, vue-router

--- vue/src/main.js
+++ vue/src/main.js
@@ -1,5 +1,14 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.js'
+
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 import './style.css'
 import App from './App.vue'
+import router from './router'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(router)
+app.mount('#app')
