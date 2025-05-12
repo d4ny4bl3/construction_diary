0361748711fd9dea3dda6b66475ef31428e53dc3
Jakub Danis <danisj91@gmail.com>
2025-05-12 23:26:15 +0200

    i18n(vue): add i18n

---
+++ vue/src/locales/index.js
@@ -0,0 +1,23 @@
+import { createI18n } from 'vue-i18n'
+import cs from './cs.json'
+
+const i18n = createI18n({
+    legacy: false,
+    locale: "cs",
+    messages: {
+        cs: cs,
+    },
+    datetimeFormats: {
+        cs: {
+            short: {
+                year: "numeric",
+                month: 'numeric',
+                day: 'numeric',
+                hour: '2-digit',
+                minute: '2-digit',
+            }
+        },
+    },
+})
+
+export default i18n
