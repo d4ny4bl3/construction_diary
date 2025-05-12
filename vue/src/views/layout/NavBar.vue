d605e93789081f83fcaf4fbec4f75afa9153a547
Jakub Danis <danisj91@gmail.com>
2025-05-12 23:28:23 +0200

    feat(vue): add NavBar.vue

---
+++ vue/src/views/layout/NavBar.vue
@@ -0,0 +1,11 @@
+<template>
+    <nav class="bg-dark text-white p-3 vh-100">
+        <div class="container-fluid">
+            <ul class="nav flex-column">
+                <li class="nav-item">
+                    <RouterLink to="/dashboard" class="nav-link text-white">Dashboard</RouterLink>
+                </li>
+            </ul>
+        </div>
+    </nav>
+</template>
