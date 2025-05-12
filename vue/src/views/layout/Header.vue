2a369017570e4b309660b4830a6a3541844053ba
Jakub Danis <danisj91@gmail.com>
2025-05-12 23:28:07 +0200

    feat(vue): add Header.vue

---
+++ vue/src/views/layout/Header.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="d-flex justify-content-between align-items-center bg-dark px-3 py-3 text-white">
+        <span class="navbar-brand">Stavební deník</span>
+        <div v-if="userStore.isLoggedIn" class="text-white">
+            Přihlášen: {{ userStore.user.username }}
+            <button @click="logout" class="btn btn-sm btn-outline-light ms-3">Odhlásit</button>
+        </div>
+  </div>
+</template>
+
+<script setup>
+    import { useRouter } from 'vue-router';
+    import { useUserStore } from '@/stores/user'
+
+    const userStore = useUserStore()
+    const router = useRouter()
+
+    const logout = async () => {
+        await userStore.logout()
+        router.push({name: "Login"})
+    }
+</script>
