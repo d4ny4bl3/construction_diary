04300ef3cd748e458f608ecc6fc0f75828be84c5
Jakub Danis <danisj91@gmail.com>
2025-05-12 23:46:45 +0200

    feat-(vue): set default web page title

--- vue/src/main.js
+++ vue/src/main.js
@@ -13,4 +13,12 @@ const app = createApp(App)
 app.use(createPinia())
 app.use(router)
 app.use(i18n)
+
+router.beforeEach((to, from, next) => {
+    const defaultTitle = "Stavební deník"
+    const translated = to.meta?.title ? i18n.global.t(to.meta.title) : null
+    document.title = translated ? `${defaultTitle} - ${translated}` : defaultTitle
+    next()
+})
+
 app.mount('#app')
