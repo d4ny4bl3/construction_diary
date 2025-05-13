3a58d19acd5f8a9d569b888400a5cd83adfe2d71
Jakub Danis <danisj91@gmail.com>
2025-05-14 01:23:31 +0200

    feat(vue): project add view

---
+++ vue/src/views/ProjectAddView.vue
@@ -0,0 +1,24 @@
+<template>
+    <h1>Project Add View</h1>
+
+    <ProjectForm @submit="createProject" :submit-label="t('utils.create')"/>
+
+</template>
+
+<script setup>
+    import ProjectForm from '@/components/ProjectForm.vue';
+    import { useI18n } from 'vue-i18n';
+    import { useProjectStore } from '@/stores/projectStore';
+
+    const { t } = useI18n()
+    const store = useProjectStore()
+
+    const createProject = async (data) => {
+        try {
+            await store.createProject(data)
+            router.push({name: "Dashboard"})
+        } catch (error) {
+            console.error("Error adding project", error)
+        }
+    }
+</script>
