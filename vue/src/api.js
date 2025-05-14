fc241c03bc50e91927c6f6fb19a41ecd69f19720
Jakub Danis <danisj91@gmail.com>
2025-05-15 01:38:15 +0200

    refactor(vue): replace axios csfr config with custom interceptor

--- vue/src/api.js
+++ vue/src/api.js
@@ -1,10 +1,16 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: 'http://localhost:8000',
-    withCredentials: true,
-    xsrfHeaderName: 'X-CSRFTOKEN',
-    xsrfCookieName: 'csrftoken'
+  baseURL: 'http://localhost:8000',
+  withCredentials: true
+})
+
+api.interceptors.request.use(config => {
+    const token = document.cookie.match(/csrftoken=([^;]+)/)?.[1]
+    if (token && ['post', 'put', 'patch', 'delete'].includes(config.method)) {
+        config.headers['X-CSRFTOKEN'] = token
+    }
+    return config
 })
 
 export default api
