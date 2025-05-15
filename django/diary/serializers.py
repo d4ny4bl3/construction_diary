c14fe2d56a5ac19f96a774f7f88651a7484e2ec7
Jakub Danis <danisj91@gmail.com>
2025-05-16 01:27:52 +0200

    feat(django): add ProjectStatusSerializer

--- django/diary/serializers.py
+++ django/diary/serializers.py
@@ -1,11 +1,18 @@
 from rest_framework import serializers
-from .models import Project
+from .models import Project, ProjectStatus
 from accounts.serializers import UserLightSerializer
+
+
+class ProjectStatusSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = ProjectStatus
+        fields = "__all__"
 
 
 class ProjectSerializer(serializers.ModelSerializer):
     user = UserLightSerializer(read_only=True)
-    slug = serializers.CharField(read_only=True)
+    status_name = serializers.CharField(source="status.name", read_only=True)
 
     class Meta:
         model = Project
