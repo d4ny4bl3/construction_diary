f1742d8cb86b7f83df80e4075225d63f8337f18a
Jakub Danis <danisj91@gmail.com>
2025-05-20 00:02:01 +0200

    feat(vue): add redirection to login on 401 and 403

--- vue/src/api.js
+++ vue/src/api.js
@@ -1,4 +1,5 @@
 import axios from 'axios'
+import router from '@/router'
 
 const api = axios.create({
   baseURL: 'http://localhost:8000',
@@ -12,5 +13,15 @@ api.interceptors.request.use(config => {
     }
     return config
 })
+
+api.interceptors.response.use(
+    response => response,
+    error => {
+      if (error.response && [401, 403].includes(error.response.status)) {
+        router.push({ name: 'login' })
+      }
+      return Promise.reject(error)
+    }
+)
 
 export default api
