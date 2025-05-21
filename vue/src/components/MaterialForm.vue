ae7960864c2ceaad6da259f9874554ca63a178e0
Jakub Danis <danisj91@gmail.com>
2025-05-22 00:13:42 +0200

    feat(vue): add MaterialForm

---
+++ vue/src/components/MaterialForm.vue
@@ -0,0 +1,77 @@
+<template>
+    <form class="row g-3 p-3" ref="form" @submit.prevent="submit">
+        <div class="col-6">
+            <label for="name" class="form-label fw-bold">Nazev</label>
+            <input type="text" class="form-control" id="name" v-model="name" required>
+        </div>
+        <div class="col-6">
+            <label for="unit" class="form-label fw-bold">Jednotka</label>
+            <select class="form-select" v-model="unit">
+                <option
+                    v-for="unit in store.units"
+                    :key="unit.id"
+                    :value="unit.id"
+                    required
+                >
+                    {{ unit.name }}
+                </option>
+            </select>
+        </div>
+
+        <div class="my-4">
+            <button type="submit" class="btn btn-primary w-100 py-3 fw-bold fs-5 text-white">{{ label }}</button>
+        </div>
+    </form>
+</template>
+
+<script setup>
+    import { useMaterialStore } from '@/stores/materialStore';
+    import { ref, watch, computed, onMounted } from 'vue';
+    import { useI18n } from 'vue-i18n';
+
+    const props = defineProps({
+        material: {
+            type: Object,
+            default: null
+        },
+        submitLabel: {
+            type: String,
+            default: ""
+        }
+    })
+    const emit = defineEmits(["submit"])
+
+    const store = useMaterialStore()
+    const { t } = useI18n()
+
+    onMounted(async () => {
+        try {
+            await store.fetchUnits()
+        } catch (error) {
+            console.error("Error loading units.", error)
+        }
+    })
+
+    const name = ref("")
+    const unit = ref("")
+    const label = computed(() =>
+        props.submitLabel || t("utils.create")
+    )
+
+    watch(() => props.material,
+        (newVal) => {
+            if (newVal) {
+                name.value = newVal.name || ""
+                unit.value = newVal.unit || ""
+            }
+        },
+        { immediate: true }
+    )
+
+    const submit = () => {
+        emit("submit", {
+            name: name.value,
+            unit: unit.value
+        })
+    }
+</script>
