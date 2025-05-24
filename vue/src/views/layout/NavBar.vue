8a0f653027b906c0c7e83c34ef7d737756f74705
Jakub Danis <danisj91@gmail.com>
2025-05-24 19:10:15 +0200

    i18n(vue): add translations to navbar

--- vue/src/views/layout/NavBar.vue
+++ vue/src/views/layout/NavBar.vue
@@ -3,24 +3,29 @@
         <div class="container-fluid">
             <ul class="nav flex-column">
                 <li class="nav-item">
-                    <RouterLink to="/dashboard" class="nav-link text-white">Dashboard</RouterLink>
+                    <RouterLink to="/dashboard" class="nav-link text-white">{{ t("navbar.dashboard") }}</RouterLink>
                 </li>
             </ul>
             <ul class="nav flex-column">
                 <li class="nav-item">
-                    <RouterLink :to="{ name: 'ProjectListView' }" class="nav-link text-white">Projekty</RouterLink>
+                    <RouterLink :to="{ name: 'ProjectListView' }" class="nav-link text-white">{{ t("navbar.projects") }}</RouterLink>
                 </li>
             </ul>
             <ul class="nav flex-column">
                 <li class="nav-item">
-                    <RouterLink :to="{ name: 'MaterialListView' }" class="nav-link text-white">Materialy</RouterLink>
+                    <RouterLink :to="{ name: 'MaterialListView' }" class="nav-link text-white">{{ t("navbar.materials") }}</RouterLink>
                 </li>
             </ul>
             <ul class="nav flex-column">
                 <li class="nav-item">
-                    <RouterLink :to="{ name: 'PurchaseListView' }" class="nav-link text-white">Nakupy</RouterLink>
+                    <RouterLink :to="{ name: 'PurchaseListView' }" class="nav-link text-white">{{ t("navbar.purchases") }}</RouterLink>
                 </li>
             </ul>
         </div>
     </nav>
 </template>
+
+<script setup>
+import { useI18n } from 'vue-i18n';
+const { t } = useI18n()
+</script>
