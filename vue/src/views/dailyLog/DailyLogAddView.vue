4ed57bf41788aa00cd6b1fdefe17496b25fc1b28
Jakub Danis <danisj91@gmail.com>
2025-05-28 23:11:25 +0200

    feat(vue): add createDailyLog to DailyLogView

--- vue/src/views/dailyLog/DailyLogAddView.vue
+++ vue/src/views/dailyLog/DailyLogAddView.vue
@@ -1,16 +1,30 @@
 <template>
     <h2>DailyLog Add view</h2>
 
-    <DailyLogForm />
-
-    <div class="my-4">
-            <button type="submit" class="btn btn-primary w-100 py-3 fw-bold fs-5 text-white">{{ t("utils.create") }}</button>
-        </div>
+    <DailyLogForm @submit="createDailyLog" :submit-label="t('utils.create')" />
 </template>
 
 <script setup>
 import { useI18n } from 'vue-i18n';
+import { useRouter } from 'vue-router';
+import { useDailyLogStore } from '@/stores/dailyLogStore';
 import DailyLogForm from '@/components/DailyLogForm.vue';
 
+const store = useDailyLogStore()
+const router = useRouter()
 const { t } = useI18n()
+
+const createDailyLog = async (data) => {
+    try {
+        const created = await store.createDailyLog(data)
+        router.push({
+            name: "DailyLogDetailView",
+            params: {
+                id: created.id,
+            }
+        })
+    } catch (error) {
+        console.error("Error adding daily log.", error)
+    }
+}
 </script>
