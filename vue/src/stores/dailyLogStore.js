f2b2c9e0c4b58f075619feac1f2eb066b969e8c5
Jakub Danis <danisj91@gmail.com>
2025-12-18 02:14:37 +0100

    feat(vue): add api call for material usage

--- vue/src/stores/dailyLogStore.js
+++ vue/src/stores/dailyLogStore.js
@@ -5,6 +5,8 @@ export const useDailyLogStore = defineStore("dailyLog", {
     state: () => ({
         dailyLogs: [],
         dailyLog: {},
+        materialUsages: [],
+        materialUsage: {},
     }),
 
     actions: {
@@ -29,6 +31,41 @@ export const useDailyLogStore = defineStore("dailyLog", {
         async deleteDailyLog(id) {
             await api.delete(`/daily-logs/${id}/delete/`)
             this.dailyLogs = this.dailyLogs.filter(d => d.id != id)
+        },
+
+        async fetchMaterialUsages(dailyLogId) {
+            try {
+                const response = await api.get(`/daily-logs/${dailyLogId}/usages/`)
+                this.materialUsages = response.data
+            } catch (error) {
+                console.error("Error loading material usages", error)
+            }
+        },
+        async fetchMaterialUsage(dailyLogId, usageId) {
+            try {
+                const response = await api.get(`/daily-logs/${dailyLogId}/usages/${usageId}`)
+                this.materialUsage = response.data
+            } catch (error) {
+                console.error("Error loading material usage", error)
+            }
+        },
+        async createMaterialUsage(dailyLogId, data) {
+            try {
+                const response = await api.post(`/daily-logs/${dailyLogId}/usages/add/`, data)
+                this.materialUsages.push(response.data)
+                this.materialUsage = response.data
+                return response.data
+            } catch (error) {
+                console.error("Error creating material usage.", error)
+            }
+        },
+        async deleteMaterialUsage(dailyLogId, usageId) {
+            try {
+                await api.delete(`/daily-logs/${dailyLogId}/usages/${usageId}/delete/`)
+                this.materialUsages = this.materialUsages.filter(m => m.id != usageId)
+            } catch (error) {
+                console.error("Error deleting material usage.", error)
+            }
         }
     }
 })
